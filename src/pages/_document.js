ce405dd68efd48828b2ae6c552ea19278d780f1e
joel <loophone@gmail.com>
2019-08-05 23:17:05 +0800

    Update analytics script for next.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -12,19 +13,25 @@ class MyDocument extends Document {
         <Head>
           <link  href="/static/picker.min.css" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145128640-1"></script>
-          <script>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
+            function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'UA-145128640-1');
-          </script>
+            gtag('config', '${GA_TRACKING_ID}');
+          `
+            }}
+          />
         </Head>  
         <body>
           <Main />
           <script src="/static/picker.min.js" ></script>
           <NextScript />
-          
         </body>
       </Html>
     );
