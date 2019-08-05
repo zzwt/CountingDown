8c825389e223803d96b59e03bd767600dd903d52
joel <loophone@gmail.com>
2019-08-05 20:42:17 +0800

    Aded google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,14 @@ class MyDocument extends Document {
         <Head>
           <link  href="/static/picker.min.css" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145128640-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-145128640-1');
+          </script>
         </Head>  
         <body>
           <Main />
