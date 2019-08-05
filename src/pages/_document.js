45449f0c0feeb69c2ba6ab278533476845800e1a
joel <loophone@gmail.com>
2019-08-05 20:56:24 +0800

    Update analytics script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145128640-1"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag(){dataLayer.push(arguments)}
             gtag('js', new Date());
             gtag('config', 'UA-145128640-1');
           </script>
